1da9b5448a72cb6b8f294d1e927c2681cfa378fc
PhilippEdrichCodes <philipp@edrich.codes>
2022-11-21 15:30:07 +0100

    Add CollectionView

---
+++ ReadingList/Book.swift
@@ -0,0 +1,14 @@
+//
+//  Book.swift
+//  ReadingList
+//
+//  Created by Philipp Edrich on 21.11.22.
+//
+
+import Foundation
+
+struct Book {
+    var cover: String
+    var title: String
+    var author: String
+}
--- ReadingList/ViewController.swift
+++ ReadingList/ViewController.swift
@@ -11,17 +11,33 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var greetingLabel: UILabel!
     @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
 
     var nameShowed = "Guest"
     var buttonText = "Log In"
     var loggedIn = false
       
+    let listOfBooks = [
+        Book(cover: "Anhalter", title: "Per Anhalter Durch Die Galaxis", author: "Douglas Adams"),
+        Book(cover: "NameDerRose", title: "Der Name Der Rose", author: "UmbertoEco"),
+        Book(cover: "RedOctober", title: "The Hunt For Red October", author: "Tom Clancy"),
+        Book(cover: "RedStormRising", title: "Red Storm Rising", author: "Tom Clancy"),
+        Book(cover: "WheelOfTime1", title: "Wheel Of Time", author: "Robert Jordan"),
+        Book(cover: "WheelOfTime2", title: "Wheel Of Time", author: "Robert Jordan"),
+        Book(cover: "WheelOfTime3", title: "Wheel Of Time", author: "Robert Jordan"),
+        Book(cover: "WheelOfTime4", title: "Wheel Of Time", author: "Robert Jordan")
+    ]
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         greetingLabel.text = "Welcome " + nameShowed + "!"
         loginButton.setTitle(buttonText, for: .normal)
+        
+        collectionView.dataSource = self
+        
         
         NotificationCenter.default.addObserver(self, selector: #selector(changeTexts( _ :)), name: Notification.Name(rawValue: "loggedInUser"), object: nil)
         
@@ -65,4 +81,28 @@ class ViewController: UIViewController {
         NotificationCenter.default.removeObserver( self, name: Notification.Name(rawValue: "loggedInUser"), object: nil)
     }
 
+}
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return listOfBooks.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "bookCell", for: indexPath)
+        
+        var content = cell.defaultBackgroundConfiguration()
+        content.image = UIImage(named: listOfBooks[indexPath.row].cover)
+        
+        cell.backgroundConfiguration = content
+        
+        return cell
+    }
+    
+    
 }
